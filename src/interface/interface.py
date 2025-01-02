77034e0d7467c4f9aa115a8521738e33527fca5e
HugoLRauber <hugoleite8882@gmail.com>
2025-01-02 01:54:06 +0000

    interface modificada e centrada

--- src/interface/interface.py
+++ src/interface/interface.py
@@ -1,6 +1,7 @@
 from os import system, name, environ
 import multiprocessing
 from json import dumps as json_dumps
+import shutil
 
 from map.map import Map
 from map.draw_map import draw_map
@@ -14,29 +15,64 @@ from algorithms.dario import dario
 from algorithms.dionisio import dionisio
 
 
+def print_centered(text=""):
+    """Imprime texto centrado no terminal."""
+    columns, _ = shutil.get_terminal_size()
+    print(text.center(columns))
+
+
+def input_inline(prompt=""):
+    """Exibe um prompt na mesma linha para entrada."""
+    print(prompt, end="", flush=True)
+    return input()
+
+
+def input_centered(prompt=""):
+    """Imprime um prompt centrado e lê a entrada na mesma linha."""
+    columns, _ = shutil.get_terminal_size()
+    padding = (columns - len(prompt)) // 2
+    print(" " * padding + prompt, end="", flush=True)
+    return input()
+
+
+def print_dict_centered(dict_obj):
+    """Imprime um dicionário formatado e centrado."""
+    json_output = json_dumps(dict_obj, indent=2)
+    for line in json_output.splitlines():
+        print_centered(line)
+
+
 def interface(map: Map):
     clear()
-    print("=====================================")
-    print("            Trivial Rescue           ")
-    print("=====================================\n")
+    print_centered("===================================================================")
+    print_centered("|  _____     _       _       _   ____                             |")
+    print_centered("| |_   _| __(_)_   _(_) __ _| | |  _ \\ ___  ___  ___ _   _  ___   |")
+    print_centered("|   | || '__| \\ \\ / / |/ _` | | | |_) / _ \\/ __|/ __| | | |/ _ \\  |")
+    print_centered("|   | || |  | |\\ V /| | (_| | | |  _ <  __/\\__ \\ (__| |_| |  __/  |")
+    print_centered("|   |_||_|  |_| \\_/ |_|\\__,_|_| |_| \\_\\___||___/\\___|\\__,_|\\___|  |")
+    print_centered("|                                                                 |")
+    print_centered("===================================================================\n")
 
-    print("Select an option:\n")
+    print_centered("Select an option:\n")
 
-    print("1. Draw matrix map")
-    print("2. Draw real map")
-    print("3. Get Info about city")
-    print("4. Get capitals cities")
-    print("5. Get Info about route")
+    print_centered("1. Draw matrix map")
+    print_centered("2. Draw real map")
+    print_centered("3. Get Info about city")
+    print_centered("4. Get capitals cities")
+    print_centered("5. Get Info about route\n")
 
-    print("\n6. Algorithm Depth First Search")
-    print("7. Algorithm Breadth First Search")
-    print("8. Algorithm A*")
-    print("9. Algorithm (dionisio, inventa um nome para aqui)")
-    print("10. Algorithm (à lá Dário)")
+    print_centered("6. Algorithm Depth First Search")
+    print_centered("7. Algorithm Breadth First Search")
+    print_centered("8. Algorithm A*")
+    print_centered("9. Algorithm (dionisio, inventa um nome para aqui)")
+    print_centered("10. Algorithm (à lá Dário)\n")
 
-    print("\n0. Code Examples")
+    print_centered("0. Code Examples\n")
 
-    option = input("\nOption: ")
+    option = input_centered("Option: ")
+    clear()  # Limpa a tela para centralizar os resultados subsequentes
+    print_centered(f"Option: {option}")
+    print_centered("===================================================================")
 
     if option == "1":
         job_for_another_core = multiprocessing.Process(
@@ -51,127 +87,68 @@ def interface(map: Map):
         job_for_another_core.start()
 
     elif option == "3":
-        city_id = input("\nCity ID: ")
+        city_id = input_centered("City ID: ")
 
         try:
             city = map.get_city_by_id(city_id)
 
-            if city == None:
-                print("\nCity not found")
-
+            if city is None:
+                print_centered("City not found")
             else:
-                print("\n")
-                print_dict(city)
+                print_dict_centered(city)
 
         except Exception as e:
-            print(f"\nError: {e}")
+            print_centered(f"Error: {e}")
 
         press_to_continue()
 
     elif option == "4":
         capitals = map.get_capitals()
-
-        print("\nCapitals IDs:")
-        print_dict([capital["id"] for capital in capitals])
+        print_centered("Capitals IDs:")
+        print_dict_centered([capital["id"] for capital in capitals])
 
         press_to_continue()
 
     elif option == "5":
-        city_id_1 = input("\nCity ID 1: ")
-        city_id_2 = input("City ID 2: ")
+        city_id_1 = input_centered("City ID 1: ")
+        city_id_2 = input_centered("City ID 2: ")
 
         try:
             routes = map.get_routes_between_cities(city_id_1, city_id_2)
-
-            print("\n")
-            print_dict(routes)
+            print_dict_centered(routes)
 
         except Exception as e:
-            print(f"\nError: {e}")
+            print_centered(f"Error: {e}")
 
         press_to_continue()
 
-    elif option == "6":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
+    elif option in {"6", "7", "8", "9", "10"}:
+        city_id = input_centered("Destination City ID: ")
+        groceries_tons = int(input_centered("Tons of grocery: "))
 
         try:
-            path = depth_first_search(map, city_id, groceries_tons)
-
+            algorithms = {
+                "6": depth_first_search,
+                "7": breadth_first_search,
+                "8": A_star,
+                "9": dionisio,
+                "10": dario,
+            }
+            path = algorithms[option](map, city_id, groceries_tons)
             do_you_want_draw_the_path(map, path)
 
         except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "7":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        path = breadth_first_search(map, city_id, groceries_tons)
-        do_you_want_draw_the_path(map, path)
-        try:
-            ...
-
-        except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "8":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        path = A_star(map, city_id, groceries_tons)
-        do_you_want_draw_the_path(map, path)
-        try:
-            # A_star(map, city_id, groceries_tons)
-            ...
-
-        except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "9":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        path = dionisio(map, city_id, groceries_tons)
-        do_you_want_draw_the_path(map, path)
-        try:
-            # dionisio(map, city_id, groceries_tons)
-            ...
-
-        except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "10":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        path = dario(map, city_id, groceries_tons)
-        do_you_want_draw_the_path(map, path)
-        try:
-            # dario(map, city_id, groceries_tons)
-            ...
-
-        except Exception as e:
-            print(f"\nError: {e}")
+            print_centered(f"Error: {e}")
 
         press_to_continue()
 
     elif option == "0":
-        print(map.get_city_by_id(0))
-        print(map.get_capitals())
-        # print(map.get_all_cities())
-        print(map.get_routes_between_cities(0, 1))
+        print_dict_centered(map.get_city_by_id(0))
+        print_dict_centered(map.get_capitals())
+        print_dict_centered(map.get_routes_between_cities(0, 1))
 
         route = [Car(map, 0, 1)]
-        print(sum_vehicles_cost(route))
+        print_centered(str(sum_vehicles_cost(route)))
 
         press_to_continue()
 
@@ -188,22 +165,15 @@ def clear():
 
 
 def press_to_continue():
-    input("\nPress Enter to continue...")
-
-
-def print_dict(dict):
-    print(json_dumps(dict, indent=2))
+    input_centered("Press Enter to continue...")
 
 
 def do_you_want_draw_the_path(map, path):
     if path is None:
         return
 
-    answer = input("\nDo you want to draw the path? (y/N) ")
+    answer = input_centered("Do you want to draw the path? (y/N) ")
 
-    if answer == "y":
+    if answer.lower() == "y":
         job = multiprocessing.Process(target=draw_map, args=(map, "real", path))
         job.start()
-    else:
-        pass
-    return
