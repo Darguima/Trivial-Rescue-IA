78413d3f526a6b0e8d21966c2c3f08d58bbab8c9
Hugo Leite Rauber <131184243+HugoLRauber@users.noreply.github.com>
2025-01-03 13:33:57 +0000

    Hr/interface (#7)
    * BFS added
    
    * bfs e dfs corrigidos para ter em conta a capita
    
    * interface modificada e centrada
    
    * .
    
    ---------
    
    Co-authored-by: Darguima <dsgdevbraga@gmail.com>

--- src/interface/interface.py
+++ src/interface/interface.py
@@ -1,6 +1,7 @@
 from os import system, name, environ
 import multiprocessing
 from json import dumps as json_dumps
+import shutil
 
 from map.map import Map
 from map.draw_map import draw_map
@@ -15,32 +16,75 @@ from algorithms.bidirectional_bfs import bidirectional_bfs
 from algorithms.bidirectional_dfs import bidirectional_dfs
 
 
+def print_centered(text="", break_line_before=False):
+    """Imprime texto centrado no terminal."""
+    columns, _ = shutil.get_terminal_size()
+    if break_line_before:
+        print("\n")
+    print(text.center(columns))
+
+
+def input_inline(prompt=""):
+    """Exibe um prompt na mesma linha para entrada."""
+    print(prompt, end="", flush=True)
+    return input()
+
+
+def input_centered(prompt="", break_line_before=False):
+    """Imprime um prompt centrado e lê a entrada na mesma linha."""
+    columns, _ = shutil.get_terminal_size()
+    padding = (columns - len(prompt)) // 2
+
+    if break_line_before:
+        print("\n")
+
+    print(" " * padding + prompt, end="", flush=True)
+    return input()
+
+
+def print_dict_centered(dict_obj):
+    """Imprime um dicionário formatado e centrado."""
+    json_output = json_dumps(dict_obj, indent=2)
+    for line in json_output.splitlines():
+        print_centered(line)
+
+
 def interface(map: Map):
     clear()
-    print("=====================================")
-    print("            Trivial Rescue           ")
-    print("=====================================\n")
+    print_centered("===================================================================")
+    print_centered("|  _____     _       _       _   ____                             |")
+    print_centered("| |_   _| __(_)_   _(_) __ _| | |  _ \\ ___  ___  ___ _   _  ___   |")
+    print_centered("|   | || '__| \\ \\ / / |/ _` | | | |_) / _ \\/ __|/ __| | | |/ _ \\  |")
+    print_centered("|   | || |  | |\\ V /| | (_| | | |  _ <  __/\\__ \\ (__| |_| |  __/  |")
+    print_centered("|   |_||_|  |_| \\_/ |_|\\__,_|_| |_| \\_\\___||___/\\___|\\__,_|\\___|  |")
+    print_centered("|                                                                 |")
+    print_centered("===================================================================\n")
 
-    print("Select an option:\n")
+    print_centered("Select an option:\n")
 
-    print("1. Draw matrix map (land routes)")
-    print("2. Draw real map (land routes)")
-    print("3. Draw matrix map (sea routes)")
-    print("4. Draw matrix map (air routes)")
+    print_centered("1. Draw matrix map (land routes)")
+    print_centered("2. Draw real map (land routes)")
+    print_centered("3. Draw matrix map (sea routes)")
+    print_centered("4. Draw matrix map (air routes)")
 
-    print("\n5. Get Info about city")
-    print("6. Get capitals cities")
-    print("7. Get Info about route")
+    print_centered("5. Get Info about city", break_line_before=True)
+    print_centered("6. Get capitals cities")
+    print_centered("7. Get Info about route")
     
-    print("\n8. Algorithm Depth First Search")
-    print("9. Algorithm Breadth First Search")
-    print("10. Algorithm A*")
-    print("11. Algorithm Greedy")
-    print("12. Algorithm (à lá Dário)")
-    print("13. Algorithm Dijkstra")
-    print("14. Algorithm Bidirectional")
+    print_centered("8. Algorithm Depth First Search", break_line_before=True)
+    print_centered("9. Algorithm Breadth First Search")
+    print_centered("10. Algorithm A*")
+    print_centered("11. Algorithm Greedy")
+    print_centered("12. Algorithm (à lá Dário)")
+    print_centered("13. Algorithm Dijkstra")
+    print_centered("14. Algorithm Bidirectional DFS")
+    print_centered("15. Algorithm Bidirectional BFS")
 
-    option = input("\nOption: ")
+    option = input_centered("Option: ", break_line_before=True)
+    clear()
+
+    print_centered(f"Option: {option}")
+    print_centered("===================================================================")
 
     if option == "1":
         job_for_another_core = multiprocessing.Process(
@@ -67,159 +111,61 @@ def interface(map: Map):
         job_for_another_core.start()
 
     elif option == "5":
-        city_id = input("\nCity ID: ")
+        city_id = input_centered("\nCity ID: ")
 
         try:
             city = map.get_city_by_id(city_id)
 
-            if city == None:
-                print("\nCity not found")
-
+            if city is None:
+                print_centered("City not found")
             else:
-                print("\n")
-                print_dict(city)
+                print_dict_centered(city)
 
         except Exception as e:
-            print(f"\nError: {e}")
+            print_centered(f"Error: {e}")
 
         press_to_continue()
 
     elif option == "6":
         capitals = map.get_capitals()
-
-        print("\nCapitals IDs:")
-        print_dict([capital["id"] for capital in capitals])
+        print_centered("Capitals IDs:")
+        print_dict_centered([capital["id"] for capital in capitals])
 
         press_to_continue()
 
     elif option == "7":
-        city_id_1 = input("\nCity ID 1: ")
-        city_id_2 = input("City ID 2: ")
+        city_id_1 = input_centered("\nCity ID 1: ")
+        city_id_2 = input_centered("City ID 2: ")
 
         try:
             routes = map.get_routes_between_cities(city_id_1, city_id_2)
-
-            print("\n")
-            print_dict(routes)
+            print_dict_centered(routes)
 
         except Exception as e:
-            print(f"\nError: {e}")
+            print_centered(f"Error: {e}")
 
         press_to_continue()
 
-    elif option == "8":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        try:
-            path = depth_first_search(map, city_id, groceries_tons)
-
-            do_you_want_draw_the_path(map, path)
-
-        except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "9":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
- 
-        try:
-            path = breadth_first_search(map, city_id, groceries_tons)
-            do_you_want_draw_the_path(map, path)
-
-        except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "10":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        try:
-            path = A_star(map, city_id, groceries_tons)
-            do_you_want_draw_the_path(map, path)
-
-        except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "11":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        try:
-            path = greedy(map, city_id, groceries_tons)
-            do_you_want_draw_the_path(map, path)
-
-        except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "12":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        path = dario(map, city_id, groceries_tons)
-        do_you_want_draw_the_path(map, path)
-        try:
-            # dario(map, city_id, groceries_tons)
-            ...
-
-        except Exception as e:
-            print(f"\nError: {e}")
-
-        press_to_continue()
-
-    elif option == "13":
-        city_id = input("\nDestination City ID: ")
-        groceries_tons = int(input("\nTons of grocery: "))
-
-        path = dijkstra(map, city_id, groceries_tons)
-        do_you_want_draw_the_path(map, path)
-        try:
-            # dario(map, city_id, groceries
-            ...
-        except Exception as e:
-            print(f"\nError: {e}")
-        press_to_continue()
-
-    elif option == "14":
-    # Ask for the type of search: BFS or DFS
-        search_type = input("\nChoose search type: 1) BFS or 2) DFS: ")
-
-        if search_type == "1":
-            city_id = input("\nDestination City ID: ")
-            groceries_tons = int(input("\nTons of groceries: "))
-
-            # Call bidirectional BFS
-            path = bidirectional_bfs(map, city_id, groceries_tons)
-            if path:
-                do_you_want_draw_the_path(map, path)  # Draw the path if found
-            else:
-                print("\nNo path found using BFS.")
+    elif option in {"8", "9", "10", "11", "12", "13", "14", "15"}:
+        city_id = input_centered("Destination City ID: ")
+        groceries_tons = int(input_centered("Tons of grocery: "))
         
-        elif search_type == "2":
-            city_id = input("\nDestination City ID: ")
-            groceries_tons = int(input("\nTons of groceries: "))
-
-            # Call bidirectional DFS
-            path = bidirectional_dfs(map, city_id, groceries_tons)
-            if path:
-                do_you_want_draw_the_path(map, path)  # Draw the path if found
-            else:
-                print("\nNo path found using DFS.")
-
         try:
-            # Handle additional actions or functions if needed (currently commented out)
-            # dario(map, city_id, groceries)
-            pass
+            algorithms = {
+                "8": depth_first_search,
+                "9": breadth_first_search,
+                "10": A_star,
+                "11": greedy,
+                "12": dario,
+                "13": dijkstra,
+                "14": bidirectional_dfs,
+                "15": bidirectional_bfs,
+            }
+            path = algorithms[option](map, city_id, groceries_tons)
+            do_you_want_draw_the_path(map, path)
+
         except Exception as e:
-            print(f"\nError: {e}")
+            print_centered(f"Error: {e}")
 
         press_to_continue()
 
@@ -235,22 +181,15 @@ def clear():
 
 
 def press_to_continue():
-    input("\nPress Enter to continue...")
-
-
-def print_dict(dict):
-    print(json_dumps(dict, indent=2))
+    input_centered("Press Enter to continue...")
 
 
 def do_you_want_draw_the_path(map, path):
     if path is None:
         return
 
-    answer = input("\nDo you want to draw the path? (y/N) ")
+    answer = input_centered("Do you want to draw the path? (y/N) ")
 
     if answer == "y":
         job = multiprocessing.Process(target=draw_map, args=(map, "real", "land", path))
         job.start()
-    else:
-        pass
-    return
